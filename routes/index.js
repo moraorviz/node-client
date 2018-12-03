0854adc6f501de7a81be18e9a50f50dde894c1d9
Mario <moraorviz@gmail.com>
2018-12-03 02:32:04 +0100

    update ddbb credentials

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'mario',
   password : 'admin',
-  database : 'test'
+  database : 'produccion'
 });
 
 /* GET home page. */
